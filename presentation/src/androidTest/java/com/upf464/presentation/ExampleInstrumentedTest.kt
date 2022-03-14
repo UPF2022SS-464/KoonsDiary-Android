8950f3b228eba301877c84d2bdb78599036d133c
Keich.Park <svclaw2000@gmail.com>
2022-03-14 15:57:30 +0900

    build: KDA-16 App Layer 의존성 추가

--- presentation/src/androidTest/java/com/upf464/presentation/ExampleInstrumentedTest.kt
+++ presentation/src/androidTest/java/com/upf464/presentation/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.upf464.presentation", appContext.packageName)
+        assertEquals("com.upf464.presentation.test", appContext.packageName)
     }
 }
