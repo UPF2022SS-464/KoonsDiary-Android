2b70252ddea00566d0d3544588ed5261e4989bb8
Keich.Park <svclaw2000@gmail.com>
2022-03-15 15:45:47 +0900

    feat: KDA-18 이메일 로그인 UseCase 추가

---
+++ domain/src/main/java/com/upf464/koonsdiary/domain/di/UserUseCaseModule.kt
@@ -0,0 +1,20 @@
+package com.upf464.koonsdiary.domain.di
+
+import com.upf464.koonsdiary.domain.request.LoginWithUsernameRequest
+import com.upf464.koonsdiary.domain.response.EmptyResponse
+import com.upf464.koonsdiary.domain.usecase.LoginWithUsernameUseCase
+import com.upf464.koonsdiary.domain.usecase.ResultUseCase
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+internal abstract class UserUseCaseModule {
+
+    @Binds
+    abstract fun bindLoginWithUsernameUseCase(
+        useCase: LoginWithUsernameUseCase
+    ) : ResultUseCase<LoginWithUsernameRequest, EmptyResponse>
+}
