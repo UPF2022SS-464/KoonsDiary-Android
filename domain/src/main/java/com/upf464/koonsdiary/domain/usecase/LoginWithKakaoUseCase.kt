87b62313f2be4af7e00fffc071412daea37c5f85
Keich.Park <svclaw2000@gmail.com>
2022-03-17 17:08:43 +0900

    feat: KDA-19 EmptyRequest 제거

--- domain/src/main/java/com/upf464/koonsdiary/domain/usecase/LoginWithKakaoUseCase.kt
+++ domain/src/main/java/com/upf464/koonsdiary/domain/usecase/LoginWithKakaoUseCase.kt
@@ -1,16 +1,16 @@
 package com.upf464.koonsdiary.domain.usecase
 
 import com.upf464.koonsdiary.domain.common.flatMap
-import com.upf464.koonsdiary.domain.request.EmptyRequest
+import com.upf464.koonsdiary.domain.request.LoginWithKakaoRequest
 import com.upf464.koonsdiary.domain.response.EmptyResponse
 import com.upf464.koonsdiary.domain.service.KakaoService
 import javax.inject.Inject
 
 internal class LoginWithKakaoUseCase @Inject constructor(
     private val kakaoService: KakaoService
-) : ResultUseCase<EmptyRequest, EmptyResponse> {
+) : ResultUseCase<LoginWithKakaoRequest, EmptyResponse> {
 
-    override suspend fun invoke(request: EmptyRequest): Result<EmptyResponse> {
+    override suspend fun invoke(request: LoginWithKakaoRequest): Result<EmptyResponse> {
         return kakaoService.loginWithKakao().flatMap {
             Result.success(EmptyResponse)
         }
