4cf85174c7cb3edd055be3c32241e08462bef9cf
Keich.Park <svclaw2000@gmail.com>
2022-03-17 15:30:37 +0900

    feat: KDA-19 카카오 SDK 초기화

--- app/src/main/java/com/upf464/koonsdiary/App.kt
+++ app/src/main/java/com/upf464/koonsdiary/App.kt
@@ -1,7 +1,14 @@
 package com.upf464.koonsdiary
 
 import android.app.Application
+import com.kakao.sdk.common.KakaoSdk
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class App : Application()
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        KakaoSdk.init(this, getString(R.string.kakaoNativeAppKey))
+    }
+}
