67ad5a7a5be7adf0a8947dd4d22e1115e1aad9da
Keich.Park <svclaw2000@gmail.com>
2022-03-17 16:58:35 +0900

    feat: KDA-19 카카오 로그인 관련 에러 추가

--- kakao/src/main/java/com/upf464/koonsdiary/kakao/service/KakaoServiceImpl.kt
+++ kakao/src/main/java/com/upf464/koonsdiary/kakao/service/KakaoServiceImpl.kt
@@ -1,8 +1,12 @@
 package com.upf464.koonsdiary.kakao.service
 
 import android.content.Context
+import com.kakao.sdk.common.model.AuthError
+import com.kakao.sdk.common.model.ClientError
 import com.kakao.sdk.user.UserApiClient
+import com.upf464.koonsdiary.domain.common.errorMap
 import com.upf464.koonsdiary.domain.service.KakaoService
+import com.upf464.koonsdiary.kakao.mapper.toDomain
 import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.suspendCancellableCoroutine
 import javax.inject.Inject
@@ -12,20 +16,30 @@ internal class KakaoServiceImpl @Inject constructor(
     @ApplicationContext private val context: Context
 ): KakaoService {
 
-    override suspend fun loginWithKakao(): Result<Unit> = suspendCancellableCoroutine { cancellable ->
-        with(UserApiClient.instance) {
-            if (isKakaoTalkLoginAvailable(context)) {
-                loginWithKakaoTalk(context) { token, error ->
-                    error?.let {
-                        cancellable.resume(Result.failure(error))
-                    } ?: cancellable.resume(Result.success(Unit))
+    override suspend fun loginWithKakao(): Result<Unit> {
+        val result = suspendCancellableCoroutine<Result<Unit>> { cancellable ->
+            with(UserApiClient.instance) {
+                if (isKakaoTalkLoginAvailable(context)) {
+                    loginWithKakaoTalk(context) { token, error ->
+                        error?.let {
+                            cancellable.resume(Result.failure(error))
+                        } ?: cancellable.resume(Result.success(Unit))
+                    }
+                } else {
+                    loginWithKakaoAccount(context) { token, error ->
+                        error?.let {
+                            cancellable.resume(Result.failure(error))
+                        } ?: cancellable.resume(Result.success(Unit))
+                    }
                 }
-            } else {
-                loginWithKakaoAccount(context) { token, error ->
-                    error?.let {
-                        cancellable.resume(Result.failure(error))
-                    } ?: cancellable.resume(Result.success(Unit))
-                }
+            }
+        }
+
+        return result.errorMap { error ->
+            when (error) {
+                is ClientError -> error.toDomain()
+                is AuthError -> error.toDomain()
+                else -> Exception(error)
             }
         }
     }
