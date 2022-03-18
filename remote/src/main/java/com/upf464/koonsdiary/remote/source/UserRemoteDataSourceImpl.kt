eb966f4a587c6da6eb1f641a36e863fe8bcbb105
Keich.Park <svclaw2000@gmail.com>
2022-03-18 15:16:51 +0900

    feat: KDA-21 이메일 회원가입 관련 Data Source 추가

--- remote/src/main/java/com/upf464/koonsdiary/remote/source/UserRemoteDataSourceImpl.kt
+++ remote/src/main/java/com/upf464/koonsdiary/remote/source/UserRemoteDataSourceImpl.kt
@@ -1,5 +1,6 @@
 package com.upf464.koonsdiary.remote.source
 
+import com.upf464.koonsdiary.data.model.SignUpUserData
 import com.upf464.koonsdiary.data.source.UserRemoteDataSource
 import javax.inject.Inject
 
@@ -9,5 +10,15 @@ internal class UserRemoteDataSourceImpl @Inject constructor(
     override suspend fun loginWithUsername(username: String, password: String): Result<Unit> {
         // TODO("서버 완료 되면 진행")
         return Result.success(Unit)
+    }
+
+    override suspend fun signUpWithAccount(user: SignUpUserData): Result<Unit> {
+        // TODO("서버 완료 되면 진행")
+        return Result.success(Unit)
+    }
+
+    override suspend fun generateSaltOf(username: String): Result<String> {
+        // TODO("서버 완료 되면 진행")
+        return Result.success("")
     }
 }
