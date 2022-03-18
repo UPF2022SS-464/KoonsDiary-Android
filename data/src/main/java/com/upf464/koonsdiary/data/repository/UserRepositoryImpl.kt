51d29efc8c4162a3385f56d58d8033541b871c55
Keich.Park <svclaw2000@gmail.com>
2022-03-18 15:14:07 +0900

    feat: KDA-21 이메일 회원가입 관련 Repository 추가

--- data/src/main/java/com/upf464/koonsdiary/data/repository/UserRepositoryImpl.kt
+++ data/src/main/java/com/upf464/koonsdiary/data/repository/UserRepositoryImpl.kt
@@ -1,6 +1,8 @@
 package com.upf464.koonsdiary.data.repository
 
+import com.upf464.koonsdiary.data.mapper.toData
 import com.upf464.koonsdiary.data.source.UserRemoteDataSource
+import com.upf464.koonsdiary.domain.model.SignUpUser
 import com.upf464.koonsdiary.domain.repository.UserRepository
 import javax.inject.Inject
 
@@ -10,5 +12,13 @@ internal class UserRepositoryImpl @Inject constructor(
 
     override suspend fun loginWithUsername(username: String, password: String): Result<Unit> {
         return remote.loginWithUsername(username, password)
+    }
+
+    override suspend fun signUpWithAccount(user: SignUpUser): Result<Unit> {
+        return remote.signUpWithAccount(user.toData())
+    }
+
+    override suspend fun generateSaltOf(username: String): Result<String> {
+        return remote.generateSaltOf(username)
     }
 }
