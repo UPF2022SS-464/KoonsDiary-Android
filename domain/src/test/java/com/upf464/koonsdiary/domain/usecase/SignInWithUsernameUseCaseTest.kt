cff8083ddf3f28698a3ad6a99f58d9cd94779c2e
Keich.Park <svclaw2000@gmail.com>
2022-03-18 16:09:41 +0900

    test: KDA-21 이메일 로그인 테스트 수정

--- domain/src/test/java/com/upf464/koonsdiary/domain/usecase/SignInWithUsernameUseCaseTest.kt
+++ domain/src/test/java/com/upf464/koonsdiary/domain/usecase/SignInWithUsernameUseCaseTest.kt
@@ -1,14 +1,14 @@
 package com.upf464.koonsdiary.domain.usecase
 
 import com.upf464.koonsdiary.domain.common.HashGenerator
+import com.upf464.koonsdiary.domain.error.SignInError
 import com.upf464.koonsdiary.domain.repository.UserRepository
 import com.upf464.koonsdiary.domain.request.SignInWithUsernameRequest
 import io.mockk.coEvery
 import io.mockk.every
 import io.mockk.mockkClass
 import kotlinx.coroutines.runBlocking
-import org.junit.Assert.assertFalse
-import org.junit.Assert.assertTrue
+import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Test
 
@@ -27,12 +27,16 @@ class SignInWithUsernameUseCaseTest {
 
     @Test
     fun invoke_correctUsernameAndPassword_isSuccess(): Unit = runBlocking {
+        coEvery {
+            userRepository.fetchSaltOf("username")
+        } returns Result.success("salt")
+
         every {
-            hashGenerator.hashPasswordWithSalt("username", "password")
-        } returns "password"
+            hashGenerator.hashPasswordWithSalt("password", "salt")
+        } returns "passwordWithSalt"
 
         coEvery {
-            userRepository.signInWithUsername("username", "password")
+            userRepository.signInWithUsername("username", "passwordWithSalt")
         } returns Result.success(Unit)
 
         val result = useCase(SignInWithUsernameRequest("username", "password"))
@@ -42,16 +46,21 @@ class SignInWithUsernameUseCaseTest {
 
     @Test
     fun invoke_incorrectUsernameAndPassword_isFailure(): Unit = runBlocking {
+        coEvery {
+            userRepository.fetchSaltOf("username")
+        } returns Result.success("salt")
+
         every {
-            hashGenerator.hashPasswordWithSalt("username", "password")
-        } returns "password"
+            hashGenerator.hashPasswordWithSalt("password", "salt")
+        } returns "passwordWithSalt"
 
         coEvery {
-            userRepository.signInWithUsername("username", "password")
-        } returns Result.failure(Exception())
+            userRepository.signInWithUsername("username", "passwordWithSalt")
+        } returns Result.failure(SignInError.IncorrectUsernameOrPassword)
 
         val result = useCase(SignInWithUsernameRequest("username", "password"))
 
         assertFalse(result.isSuccess)
+        assertEquals(SignInError.IncorrectUsernameOrPassword, result.exceptionOrNull())
     }
 }
