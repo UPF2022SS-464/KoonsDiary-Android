b0dd93874cc2a41720bb3d7954b2dd5825ed99d9
Keich.Park <svclaw2000@gmail.com>
2022-03-23 18:43:15 +0900

    feat: KDA-24 일기 Repository 추가

--- data/src/main/java/com/upf464/koonsdiary/data/di/RepositoryModule.kt
+++ data/src/main/java/com/upf464/koonsdiary/data/di/RepositoryModule.kt
@@ -1,6 +1,8 @@
 package com.upf464.koonsdiary.data.di
 
+import com.upf464.koonsdiary.data.repository.DiaryRepositoryImpl
 import com.upf464.koonsdiary.data.repository.UserRepositoryImpl
+import com.upf464.koonsdiary.domain.repository.DiaryRepository
 import com.upf464.koonsdiary.domain.repository.UserRepository
 import dagger.Binds
 import dagger.Module
@@ -15,4 +17,9 @@ internal abstract class RepositoryModule {
     abstract fun bindUserRepository(
         repository: UserRepositoryImpl
     ): UserRepository
+
+    @Binds
+    abstract fun bindDiaryRepository(
+        repository: DiaryRepositoryImpl
+    ): DiaryRepository
 }
