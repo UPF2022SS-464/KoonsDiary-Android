f79b57f5e65684dea0498e0b2886c99768582c47
Keich.Park <svclaw2000@gmail.com>
2022-03-24 17:00:53 +0900

    feat: KDA-24 일기 추가 repository 및 data source 추가

---
+++ data/src/main/java/com/upf464/koonsdiary/data/model/DiaryData.kt
@@ -0,0 +1,20 @@
+package com.upf464.koonsdiary.data.model
+
+import java.time.LocalDate
+import java.time.LocalDateTime
+
+data class DiaryData(
+    val id: Int = 0,
+    val date: LocalDate = LocalDate.now(),
+    val content: String = "",
+    val sentiment: Int = 0,
+    val imageList: List<ImageData> = emptyList(),
+    val lastModifiedDate: LocalDateTime = LocalDateTime.now(),
+    val createdDate: LocalDateTime = LocalDateTime.now()
+) {
+
+    data class ImageData(
+        val imagePath: String,
+        val comment: String
+    )
+}
--- data/src/main/java/com/upf464/koonsdiary/data/source/DiaryRemoteDataSource.kt
+++ data/src/main/java/com/upf464/koonsdiary/data/source/DiaryRemoteDataSource.kt
@@ -1,6 +1,10 @@
 package com.upf464.koonsdiary.data.source
 
+import com.upf464.koonsdiary.data.model.DiaryData
+
 interface DiaryRemoteDataSource {
 
     suspend fun fetchSentimentOf(content: String): Result<Int>
+
+    suspend fun addDiary(diary: DiaryData): Result<Int>
 }
