2f7e907985913c534a897145e540e549822bc60f
Keich.Park <svclaw2000@gmail.com>
2022-03-24 16:14:24 +0900

    feat: KDA-24 일기 추가 UseCase 추가

--- domain/src/main/java/com/upf464/koonsdiary/domain/repository/DiaryRepository.kt
+++ domain/src/main/java/com/upf464/koonsdiary/domain/repository/DiaryRepository.kt
@@ -1,8 +1,11 @@
 package com.upf464.koonsdiary.domain.repository
 
+import com.upf464.koonsdiary.domain.model.Diary
 import com.upf464.koonsdiary.domain.model.Sentiment
 
 interface DiaryRepository {
 
     suspend fun fetchSentimentOf(content: String): Result<Sentiment>
+
+    suspend fun addDiary(diary: Diary): Result<Int>
 }
---
+++ domain/src/main/java/com/upf464/koonsdiary/domain/response/AddDiaryResponse.kt
@@ -0,0 +1,5 @@
+package com.upf464.koonsdiary.domain.response
+
+data class AddDiaryResponse(
+    val diaryId: Int
+) : Response
