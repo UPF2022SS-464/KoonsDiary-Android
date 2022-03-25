4dbbc588d7b1212d3e97625c5bddedb7b000cdc6
JOLLA99 <yoon6148@daum.net>
2022-03-25 16:43:03 +0900

    feat: KDA-32 서버로부터 질문받기

--- data/src/main/java/com/upf464/koonsdiary/data/source/CottonRemoteDataSource.kt
+++ data/src/main/java/com/upf464/koonsdiary/data/source/CottonRemoteDataSource.kt
@@ -2,5 +2,5 @@ package com.upf464.koonsdiary.data.source
 
 interface CottonRemoteDataSource {
 
-    //suspend fun GetQuestion(id: String, questionKR: String, questionUS: String): Result<String>
+    suspend fun GetQuestion(id: String, questionKR: String, questionUS: String): Result<String>
 }
