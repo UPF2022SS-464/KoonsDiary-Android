9bf43f81a6fe40294792ab989ccd15c5ff81d750
Keich.Park <svclaw2000@gmail.com>
2022-03-25 17:25:36 +0900

    feat: KDA-27 감정 분석 검증 로직 추가

--- domain/src/main/java/com/upf464/koonsdiary/domain/usecase/AnalyzeSentimentUseCase.kt
+++ domain/src/main/java/com/upf464/koonsdiary/domain/usecase/AnalyzeSentimentUseCase.kt
@@ -1,16 +1,23 @@
 package com.upf464.koonsdiary.domain.usecase
 
+import com.upf464.koonsdiary.domain.common.DiaryValidator
 import com.upf464.koonsdiary.domain.common.flatMap
+import com.upf464.koonsdiary.domain.error.DiaryError
 import com.upf464.koonsdiary.domain.repository.DiaryRepository
 import com.upf464.koonsdiary.domain.request.AnalyzeSentimentRequest
 import com.upf464.koonsdiary.domain.response.AnalyzeSentimentResponse
 import javax.inject.Inject
 
 internal class AnalyzeSentimentUseCase @Inject constructor(
-    private val diaryRepository: DiaryRepository
+    private val diaryRepository: DiaryRepository,
+    private val validator: DiaryValidator
 ) : ResultUseCase<AnalyzeSentimentRequest, AnalyzeSentimentResponse> {
 
     override suspend fun invoke(request: AnalyzeSentimentRequest): Result<AnalyzeSentimentResponse> {
+        if (!validator.validateContent(request.content)) {
+            return Result.failure(DiaryError.EmptyContent)
+        }
+
         return diaryRepository.fetchSentimentOf(request.content).flatMap { sentiment ->
             Result.success(AnalyzeSentimentResponse(sentiment))
         }
