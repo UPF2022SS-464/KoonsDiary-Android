f15f886eff818d69b3929a4bc3d33f83cbee6501
Keich.Park <svclaw2000@gmail.com>
2022-03-25 19:14:40 +0900

    test: KDA-25 일기 수정 테스트 추가

---
+++ domain/src/test/java/com/upf464/koonsdiary/domain/usecase/UpdateDiaryUseCaseTest.kt
@@ -0,0 +1,102 @@
+package com.upf464.koonsdiary.domain.usecase
+
+import com.upf464.koonsdiary.domain.common.DiaryValidator
+import com.upf464.koonsdiary.domain.error.DiaryError
+import com.upf464.koonsdiary.domain.model.Sentiment
+import com.upf464.koonsdiary.domain.repository.DiaryRepository
+import com.upf464.koonsdiary.domain.request.UpdateDiaryRequest
+import io.mockk.coEvery
+import io.mockk.every
+import io.mockk.mockkClass
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Test
+import java.time.LocalDate
+
+class UpdateDiaryUseCaseTest {
+
+    private lateinit var validator: DiaryValidator
+    private lateinit var diaryRepository: DiaryRepository
+    private lateinit var useCase: UpdateDiaryUseCase
+
+    @Before
+    fun setup() {
+        validator = mockkClass(DiaryValidator::class)
+        diaryRepository = mockkClass(DiaryRepository::class)
+        useCase = UpdateDiaryUseCase(
+            validator = validator,
+            diaryRepository = diaryRepository
+        )
+    }
+
+    @Test
+    fun invoke_validDiary_isSuccess(): Unit = runBlocking {
+        coEvery {
+            diaryRepository.updateDiary(any())
+        } returns Result.success(1)
+
+        every {
+            validator.validateContent("content")
+        } returns true
+
+        val result = useCase(
+            UpdateDiaryRequest(
+                diaryId = 1,
+                date = LocalDate.of(2022, 3, 25),
+                content = "content",
+                sentiment = Sentiment.GOOD,
+                imageList = emptyList()
+            )
+        )
+
+        assertTrue(result.isSuccess)
+        assertEquals(1, result.getOrNull()?.diaryId)
+    }
+
+    @Test
+    fun invoke_emptyDiary_isFailure(): Unit = runBlocking {
+        every {
+            validator.validateContent("")
+        } returns false
+
+        val result = useCase(
+            UpdateDiaryRequest(
+                diaryId = 1,
+                date = LocalDate.of(2022, 3, 25),
+                content = "",
+                sentiment = Sentiment.GOOD,
+                imageList = emptyList()
+            )
+        )
+
+        Assert.assertFalse(result.isSuccess)
+        assertEquals(DiaryError.EmptyContent, result.exceptionOrNull())
+    }
+
+    @Test
+    fun invoke_incorrectDiaryId_isFailure(): Unit = runBlocking {
+        coEvery {
+            diaryRepository.updateDiary(any())
+        } returns Result.failure(DiaryError.IncorrectDiaryId)
+
+        every {
+            validator.validateContent("content")
+        } returns true
+
+        val result = useCase(
+            UpdateDiaryRequest(
+                diaryId = 1,
+                date = LocalDate.of(2022, 3, 25),
+                content = "content",
+                sentiment = Sentiment.GOOD,
+                imageList = emptyList()
+            )
+        )
+
+        Assert.assertFalse(result.isSuccess)
+        assertEquals(DiaryError.IncorrectDiaryId, result.exceptionOrNull())
+    }
+}
