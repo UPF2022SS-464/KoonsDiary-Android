8c9dfff86e29c2d748b5da08f0fdabc7d73ee33d
JOLLA99 <yoon6148@daum.net>
2022-03-28 19:04:19 +0900

    test: KDA-32 랜덤 질문 받기 테스트 추가

---
+++ domain/src/test/java/com/upf464/koonsdiary/domain/usecase/cotton/FetchRandomQuestionUseCaseTest.kt
@@ -0,0 +1,42 @@
+package com.upf464.koonsdiary.domain.usecase.cotton
+
+import com.upf464.koonsdiary.domain.model.Question
+import com.upf464.koonsdiary.domain.repository.CottonRepository
+import com.upf464.koonsdiary.domain.request.cotton.FetchRandomQuestionRequest
+import io.mockk.MockKAnnotations
+import io.mockk.coEvery
+import io.mockk.impl.annotations.MockK
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Test
+
+class FetchRandomQuestionUseCaseTest {
+
+    @MockK
+    private lateinit var cottonRepository: CottonRepository
+    private lateinit var useCase: FetchRandomQuestionUseCase
+
+    @Before
+    fun setup() {
+        MockKAnnotations.init(this)
+        useCase = FetchRandomQuestionUseCase(
+            cottonRepository = cottonRepository
+        )
+    }
+
+    @Test
+    fun invoke_nothing_isSuccess(): Unit = runBlocking {
+        val fetchedQuestion = Question(id = 1, korean = "안녕", english = "hi")
+
+        coEvery {
+            cottonRepository.fetchRandomQuestion()
+        } returns Result.success(fetchedQuestion)
+
+        val result = useCase(FetchRandomQuestionRequest)
+
+        assertTrue(result.isSuccess)
+        assertEquals(fetchedQuestion, result.getOrNull()?.question)
+    }
+}
