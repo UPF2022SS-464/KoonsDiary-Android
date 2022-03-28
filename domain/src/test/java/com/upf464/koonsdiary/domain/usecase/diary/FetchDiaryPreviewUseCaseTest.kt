bd2ff9a9673e1164460a0ec8a3ad0cf3223cc3c9
Keich.Park <svclaw2000@gmail.com>
2022-03-28 16:33:17 +0900

    test: KDA-29 프리뷰 조회 테스트 추가

---
+++ domain/src/test/java/com/upf464/koonsdiary/domain/usecase/diary/FetchDiaryPreviewUseCaseTest.kt
@@ -0,0 +1,50 @@
+package com.upf464.koonsdiary.domain.usecase.diary
+
+import com.upf464.koonsdiary.domain.error.DiaryError
+import com.upf464.koonsdiary.domain.model.DiaryPreview
+import com.upf464.koonsdiary.domain.repository.DiaryRepository
+import com.upf464.koonsdiary.domain.request.diary.FetchDiaryPreviewRequest
+import io.mockk.MockKAnnotations
+import io.mockk.coEvery
+import io.mockk.impl.annotations.MockK
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Test
+
+class FetchDiaryPreviewUseCaseTest {
+
+    @MockK private lateinit var diaryRepository: DiaryRepository
+    private lateinit var useCase: FetchDiaryPreviewUseCase
+
+    @Before
+    fun setUp() {
+        MockKAnnotations.init(this)
+        useCase = FetchDiaryPreviewUseCase(diaryRepository)
+    }
+
+    @Test
+    fun invoke_validDiaryId_isSuccess(): Unit = runBlocking {
+        coEvery {
+            diaryRepository.fetchDiaryPreview(any())
+        } returns Result.success(DiaryPreview(1, "", ""))
+
+        val result = useCase(FetchDiaryPreviewRequest(1))
+
+        assertTrue(result.isSuccess)
+        assertEquals(1, result.getOrNull()?.preview?.id)
+    }
+
+    @Test
+    fun invoke_invalidDiaryId_isFailure(): Unit = runBlocking {
+        coEvery {
+            diaryRepository.fetchDiaryPreview(any())
+        } returns Result.failure(DiaryError.InvalidDiaryId)
+
+        val result = useCase(FetchDiaryPreviewRequest(1))
+
+        assertTrue(result.isFailure)
+        assertEquals(DiaryError.InvalidDiaryId, result.exceptionOrNull())
+    }
+}
