f8464d091c7d513e77e55e983e5fdc8e053ed719
JOLLA99 <yoon6148@daum.net>
2022-03-28 18:06:56 +0900

    refactor: KDA-32 mapper 이름 변경

--- data/src/main/java/com/upf464/koonsdiary/data/repository/CottonRepositoryImpl.kt
+++ data/src/main/java/com/upf464/koonsdiary/data/repository/CottonRepositoryImpl.kt
@@ -1,13 +1,10 @@
 package com.upf464.koonsdiary.data.repository
 
 
-import com.upf464.koonsdiary.data.mapper.toDT
-import com.upf464.koonsdiary.data.model.QuestionData
+import com.upf464.koonsdiary.data.mapper.toDomain
 import com.upf464.koonsdiary.data.source.CottonRemoteDataSource
-import com.upf464.koonsdiary.data.source.UserRemoteDataSource
 import com.upf464.koonsdiary.domain.model.Question
 import com.upf464.koonsdiary.domain.repository.CottonRepository
-import com.upf464.koonsdiary.domain.response.cotton.GetRandomQuestionResponse
 import javax.inject.Inject
 
 internal class CottonRepositoryImpl @Inject constructor(
@@ -16,7 +13,7 @@ internal class CottonRepositoryImpl @Inject constructor(
 
     override suspend fun getRandomQuestion(): Result<Question> {
         return remote.getRandomQuestion().map { questiondata ->
-            questiondata.toDT()
+            questiondata.toDomain()
         }
     }
 }
