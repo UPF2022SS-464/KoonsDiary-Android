0d36f4a0c59ca1c86cbaacb78be538efc885fe19
Keich.Park <svclaw2000@gmail.com>
2022-03-30 16:11:05 +0900

    test: KDA-61 공유그룹 삭제 테스트 추가

---
+++ domain/src/test/java/com/upf464/koonsdiary/domain/usecase/share/DeleteGroupUseCaseTest.kt
@@ -0,0 +1,48 @@
+package com.upf464.koonsdiary.domain.usecase.share
+
+import com.upf464.koonsdiary.domain.error.ShareError
+import com.upf464.koonsdiary.domain.repository.ShareRepository
+import com.upf464.koonsdiary.domain.request.share.DeleteGroupRequest
+import io.mockk.MockKAnnotations
+import io.mockk.coEvery
+import io.mockk.impl.annotations.MockK
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Test
+
+class DeleteGroupUseCaseTest {
+
+    @MockK private lateinit var shareRepository: ShareRepository
+    private lateinit var useCase: DeleteGroupUseCase
+
+    @Before
+    fun setup() {
+        MockKAnnotations.init(this)
+        useCase = DeleteGroupUseCase(shareRepository)
+    }
+
+    @Test
+    fun invoke_validGroupId_isSuccess(): Unit = runBlocking {
+        coEvery {
+            shareRepository.deleteGroup(1)
+        } returns Result.success(Unit)
+
+        val result = useCase(DeleteGroupRequest(1))
+
+        assertTrue(result.isSuccess)
+    }
+
+    @Test
+    fun invoke_invalidGroupId_isFailure(): Unit = runBlocking {
+        coEvery {
+            shareRepository.deleteGroup(1)
+        } returns Result.failure(ShareError.InvalidGroupId)
+
+        val result = useCase(DeleteGroupRequest(1))
+
+        assertTrue(result.isFailure)
+        assertEquals(ShareError.InvalidGroupId, result.exceptionOrNull())
+    }
+}
