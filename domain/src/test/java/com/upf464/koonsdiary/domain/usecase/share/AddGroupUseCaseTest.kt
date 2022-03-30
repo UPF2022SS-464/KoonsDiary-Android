fefe55804ecf6bb16b22b5a4277c5431c5420323
Keich.Park <svclaw2000@gmail.com>
2022-03-30 14:36:49 +0900

    test: KDA-34 공유그룹 생성 테스트 추가

---
+++ domain/src/test/java/com/upf464/koonsdiary/domain/usecase/share/AddGroupUseCaseTest.kt
@@ -0,0 +1,56 @@
+package com.upf464.koonsdiary.domain.usecase.share
+
+import com.upf464.koonsdiary.domain.common.GroupValidator
+import com.upf464.koonsdiary.domain.error.ShareError
+import com.upf464.koonsdiary.domain.repository.ShareRepository
+import com.upf464.koonsdiary.domain.request.share.AddGroupRequest
+import io.mockk.MockKAnnotations
+import io.mockk.coEvery
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Test
+
+class AddGroupUseCaseTest {
+
+    @MockK private lateinit var shareRepository: ShareRepository
+    @MockK private lateinit var groupValidator: GroupValidator
+    private lateinit var useCase: AddGroupUseCase
+
+    @Before
+    fun setup() {
+        MockKAnnotations.init(this)
+        useCase = AddGroupUseCase(shareRepository, groupValidator)
+    }
+
+    @Test
+    fun invoke_validGroupInformation_returnsGroupId(): Unit = runBlocking {
+        every {
+            groupValidator.isGroupNameValid(any())
+        } returns true
+
+        coEvery {
+            shareRepository.addGroup(any(), any())
+        } returns Result.success(1)
+
+        val result = useCase(AddGroupRequest("name", null, emptyList()))
+
+        assertTrue(result.isSuccess)
+        assertEquals(1, result.getOrNull()?.groupId)
+    }
+
+    @Test
+    fun invoke_invalidGroupName_throwsInvalidGroupNameError(): Unit = runBlocking {
+        every {
+            groupValidator.isGroupNameValid(any())
+        } returns false
+
+        val result = useCase(AddGroupRequest("invalid name", null, emptyList()))
+
+        assertTrue(result.isFailure)
+        assertEquals(ShareError.InvalidGroupName, result.exceptionOrNull())
+    }
+}
