3c6337d9d35f42ccb43b0f5c01fbbea93e7d1d27
JOLLA99 <yoon6148@daum.net>
2022-03-30 15:31:42 +0900

    refactor: KDA-47 Request 내 writerId 삭제

--- domain/src/main/java/com/upf464/koonsdiary/domain/request/cotton/AddQuestionAnswerRequest.kt
+++ domain/src/main/java/com/upf464/koonsdiary/domain/request/cotton/AddQuestionAnswerRequest.kt
@@ -3,7 +3,6 @@ package com.upf464.koonsdiary.domain.request.cotton
 import com.upf464.koonsdiary.domain.request.Request
 
 data class AddQuestionAnswerRequest(
-    val writerId: Int,
     val questionId: Int,
     val content: String
 ) : Request
