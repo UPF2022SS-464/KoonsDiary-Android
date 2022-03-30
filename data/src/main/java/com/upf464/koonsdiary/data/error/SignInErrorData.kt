60710c66b7e22847ee2466d1bd4c6ba13695513c
Keich.Park <svclaw2000@gmail.com>
2022-03-30 17:56:20 +0900

    feat: KDA-62 Data Layer 통합 에러 추가

--- data/src/main/java/com/upf464/koonsdiary/data/error/SignInErrorData.kt
+++ data/src/main/java/com/upf464/koonsdiary/data/error/SignInErrorData.kt
@@ -3,7 +3,7 @@ package com.upf464.koonsdiary.data.error
 sealed class SignInErrorData(
     message: String? = null,
     cause: Throwable? = null
-) : Exception(message, cause) {
+) : ErrorData(message, cause) {
 
     object IncorrectUsernameOrPassword : SignInErrorData()
 }
--- data/src/main/java/com/upf464/koonsdiary/data/repository/DiaryRepositoryImpl.kt
+++ data/src/main/java/com/upf464/koonsdiary/data/repository/DiaryRepositoryImpl.kt
@@ -1,8 +1,10 @@
 package com.upf464.koonsdiary.data.repository
 
+import com.upf464.koonsdiary.data.error.ErrorData
 import com.upf464.koonsdiary.data.mapper.toData
 import com.upf464.koonsdiary.data.mapper.toDomain
 import com.upf464.koonsdiary.data.source.DiaryRemoteDataSource
+import com.upf464.koonsdiary.domain.common.errorMap
 import com.upf464.koonsdiary.domain.model.Diary
 import com.upf464.koonsdiary.domain.model.DiaryPreview
 import com.upf464.koonsdiary.domain.model.Sentiment
@@ -16,30 +18,48 @@ internal class DiaryRepositoryImpl @Inject constructor(
     override suspend fun fetchSentimentOf(content: String): Result<Sentiment> {
         return remote.fetchSentimentOf(content).map { sentimentOrder ->
             Sentiment.values()[sentimentOrder]
+        }.errorMap { error ->
+            if (error is ErrorData) error.toDomain()
+            else Exception(error)
         }
     }
 
     override suspend fun addDiary(diary: Diary): Result<Int> {
-        return remote.addDiary(diary.toData())
+        return remote.addDiary(diary.toData()).errorMap { error ->
+            if (error is ErrorData) error.toDomain()
+            else Exception(error)
+        }
     }
 
     override suspend fun updateDiary(diary: Diary): Result<Int> {
-        return remote.updateDiary(diary.toData())
+        return remote.updateDiary(diary.toData()).errorMap { error ->
+            if (error is ErrorData) error.toDomain()
+            else Exception(error)
+        }
     }
 
     override suspend fun deleteDiary(diaryId: Int): Result<Unit> {
-        return remote.deleteDiary(diaryId)
+        return remote.deleteDiary(diaryId).errorMap { error ->
+            if (error is ErrorData) error.toDomain()
+            else Exception(error)
+        }
     }
 
     override suspend fun fetchDiary(diaryId: Int): Result<Diary> {
         return remote.fetchDiary(diaryId).map { diary ->
             diary.toDomain()
+        }.errorMap { error ->
+            if (error is ErrorData) error.toDomain()
+            else Exception(error)
         }
     }
 
     override suspend fun fetchDiaryPreview(diaryId: Int): Result<DiaryPreview> {
         return remote.fetchDiaryPreview(diaryId).map { diaryPreview ->
             diaryPreview.toDomain()
+        }.errorMap { error ->
+            if (error is ErrorData) error.toDomain()
+            else Exception(error)
         }
     }
 
@@ -48,6 +68,9 @@ internal class DiaryRepositoryImpl @Inject constructor(
             sentimentList.map { sentiment ->
                 sentiment?.let { Sentiment.values()[it] }
             }
+        }.errorMap { error ->
+            if (error is ErrorData) error.toDomain()
+            else Exception(error)
         }
     }
 }
