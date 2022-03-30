862c966efc49b50e5ee5c902fd07c6d2cecd085c
JOLLA99 <yoon6148@daum.net>
2022-03-30 15:28:35 +0900

    refactor: KDA-47 CottonRepository 공백 수정

--- domain/src/main/java/com/upf464/koonsdiary/domain/repository/CottonRepository.kt
+++ domain/src/main/java/com/upf464/koonsdiary/domain/repository/CottonRepository.kt
@@ -4,6 +4,8 @@ import com.upf464.koonsdiary.domain.model.Question
 import com.upf464.koonsdiary.domain.model.QuestionAnswer
 
 interface CottonRepository {
+
     suspend fun fetchRandomQuestion(): Result<Question>
+
     suspend fun addQuestionAnswer(questionAnswer: QuestionAnswer): Result<Int>
 }
