6d7613ce6cef665fed94fcd63af252cdd84bcdaf
Keich.Park <svclaw2000@gmail.com>
2022-03-31 16:40:45 +0900

    test: KDA-40 댓글 작성 테스트 추가

---
+++ domain/src/test/java/com/upf464/koonsdiary/domain/usecase/share/AddCommentUseCaseTest.kt
@@ -0,0 +1,57 @@
+package com.upf464.koonsdiary.domain.usecase.share
+
+import com.upf464.koonsdiary.domain.error.ShareError
+import com.upf464.koonsdiary.domain.repository.ShareRepository
+import com.upf464.koonsdiary.domain.request.share.AddCommentRequest
+import io.mockk.MockKAnnotations
+import io.mockk.coEvery
+import io.mockk.impl.annotations.MockK
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Test
+
+class AddCommentUseCaseTest {
+
+    @MockK private lateinit var shareRepository: ShareRepository
+    private lateinit var useCase: AddCommentUseCase
+
+    @Before
+    fun setup() {
+        MockKAnnotations.init(this)
+        useCase = AddCommentUseCase(shareRepository)
+    }
+
+    @Test
+    fun invoke_validComment_isSuccess(): Unit = runBlocking {
+        coEvery {
+            shareRepository.addComment(any())
+        } returns Result.success(Unit)
+
+        val result = useCase(AddCommentRequest(1, "content"))
+
+        assertTrue(result.isSuccess)
+    }
+
+    @Test
+    fun invoke_invalidDiaryId_throwsInvalidDiaryIdError(): Unit = runBlocking {
+        coEvery {
+            shareRepository.addComment(any())
+        } returns Result.failure(ShareError.InvalidDiaryId)
+
+        val result = useCase(AddCommentRequest(1, "content"))
+
+        assertTrue(result.isFailure)
+        assertEquals(ShareError.InvalidDiaryId, result.exceptionOrNull())
+    }
+
+    @Test
+    fun invoke_emptyContent_throwsEmptyContentError(): Unit = runBlocking {
+
+        val result = useCase(AddCommentRequest(1, ""))
+
+        assertTrue(result.isFailure)
+        assertEquals(ShareError.EmptyContent, result.exceptionOrNull())
+    }
+}
