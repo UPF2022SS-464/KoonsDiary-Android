00f1ab95aa7a28ece7381830d35eecb17970be3b
Keich.Park <svclaw2000@gmail.com>
2022-03-31 13:24:15 +0900

    feat: KDA-36 일기 이미지 클래스 분리

---
+++ data/src/main/java/com/upf464/koonsdiary/data/model/DiaryImageMapper.kt
@@ -0,0 +1,13 @@
+package com.upf464.koonsdiary.data.model
+
+import com.upf464.koonsdiary.domain.model.DiaryImage
+
+internal fun DiaryImage.toData() = DiaryImageData(
+    imagePath = imagePath,
+    comment = comment
+)
+
+internal fun DiaryImageData.toDomain() = DiaryImage(
+    imagePath = imagePath,
+    comment = comment
+)
---
+++ domain/src/main/java/com/upf464/koonsdiary/domain/model/DiaryImage.kt
@@ -0,0 +1,6 @@
+package com.upf464.koonsdiary.domain.model
+
+data class DiaryImage(
+    val imagePath: String,
+    val comment: String
+)
