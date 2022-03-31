f25669f0764a0d1299a4ff1a92f4096ebaa99406
Keich.Park <svclaw2000@gmail.com>
2022-03-31 15:20:06 +0900

    test: KDA-39 공유일기 조회 테스트 추가

---
+++ domain/src/test/java/com/upf464/koonsdiary/domain/usecase/share/FetchShareDiaryListUseCaseTest.kt
@@ -0,0 +1,50 @@
+package com.upf464.koonsdiary.domain.usecase.share
+
+import com.upf464.koonsdiary.domain.error.ShareError
+import com.upf464.koonsdiary.domain.model.ShareDiary
+import com.upf464.koonsdiary.domain.repository.ShareRepository
+import com.upf464.koonsdiary.domain.request.share.FetchShareDiaryListRequest
+import io.mockk.MockKAnnotations
+import io.mockk.coEvery
+import io.mockk.impl.annotations.MockK
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Test
+
+class FetchShareDiaryListUseCaseTest {
+
+    @MockK private lateinit var shareRepository: ShareRepository
+    private lateinit var useCase: FetchShareDiaryListUseCase
+
+    @Before
+    fun setup() {
+        MockKAnnotations.init(this)
+        useCase = FetchShareDiaryListUseCase(shareRepository)
+    }
+
+    @Test
+    fun invoke_validGroupId_returnsEmptyList(): Unit = runBlocking {
+        coEvery {
+            shareRepository.fetchDiaryList(1)
+        } returns Result.success(emptyList())
+
+        val result = useCase(FetchShareDiaryListRequest(1))
+
+        assertTrue(result.isSuccess)
+        assertEquals(emptyList<ShareDiary>(), result.getOrNull()?.diaryList)
+    }
+
+    @Test
+    fun invoke_invalidGroupId_throwsInvalidGroupIdError(): Unit = runBlocking {
+        coEvery {
+            shareRepository.fetchDiaryList(1)
+        } returns Result.failure(ShareError.InvalidGroupId)
+
+        val result = useCase(FetchShareDiaryListRequest(1))
+
+        assertTrue(result.isFailure)
+        assertEquals(ShareError.InvalidGroupId, result.exceptionOrNull())
+    }
+}
