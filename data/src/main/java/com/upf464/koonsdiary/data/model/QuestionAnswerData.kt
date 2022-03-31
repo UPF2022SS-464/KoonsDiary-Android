a9bd3d77617dd9714acf67ea175f58d760572e8d
JOLLA99 <yoon6148@daum.net>
2022-03-31 14:12:02 +0900

    feat: KDA-58 QuestionAnswer 값 변경 및 Reaction 모델 추가

--- data/src/main/java/com/upf464/koonsdiary/data/model/QuestionAnswerData.kt
+++ data/src/main/java/com/upf464/koonsdiary/data/model/QuestionAnswerData.kt
@@ -1,11 +1,12 @@
 package com.upf464.koonsdiary.data.model
 
+import com.upf464.koonsdiary.domain.model.Question
 import java.time.LocalDateTime
 
 data class QuestionAnswerData(
     val id: Int = 0,
     val writerId: Int = 0,
     val createdDate: LocalDateTime = LocalDateTime.now(),
-    val questionId: Int,
+    val questionId: Question,
     val content: String
 )
