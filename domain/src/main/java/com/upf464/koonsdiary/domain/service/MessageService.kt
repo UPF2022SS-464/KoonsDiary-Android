b35ed6d4f4e06b13b7d0fd1845d8bca4cf2661de
Keich.Park <svclaw2000@gmail.com>
2022-04-01 16:12:31 +0900

    feat: KDA-68 FCM 모델 및 service 추가

---
+++ domain/src/main/java/com/upf464/koonsdiary/domain/service/MessageService.kt
@@ -0,0 +1,11 @@
+package com.upf464.koonsdiary.domain.service
+
+import com.upf464.koonsdiary.domain.model.Message
+import kotlinx.coroutines.flow.Flow
+
+interface MessageService {
+
+    fun getMessageFlow(): Flow<Message>
+
+    fun setMessage(message: Message)
+}
