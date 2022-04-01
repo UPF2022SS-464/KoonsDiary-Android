369241081bd27c38e86764f8e7f33e62247c5629
Keich.Park <svclaw2000@gmail.com>
2022-04-01 17:39:25 +0900

    feat: KDA-68 로그인 성공 시 FCM 토큰 등록

--- domain/src/main/java/com/upf464/koonsdiary/domain/usecase/user/AutoSignInUseCase.kt
+++ domain/src/main/java/com/upf464/koonsdiary/domain/usecase/user/AutoSignInUseCase.kt
@@ -3,16 +3,20 @@ package com.upf464.koonsdiary.domain.usecase.user
 import com.upf464.koonsdiary.domain.common.flatMap
 import com.upf464.koonsdiary.domain.error.SignInError
 import com.upf464.koonsdiary.domain.model.SignInType
+import com.upf464.koonsdiary.domain.repository.MessageRepository
 import com.upf464.koonsdiary.domain.repository.UserRepository
 import com.upf464.koonsdiary.domain.request.user.AutoSignInRequest
 import com.upf464.koonsdiary.domain.response.EmptyResponse
 import com.upf464.koonsdiary.domain.service.KakaoService
+import com.upf464.koonsdiary.domain.service.MessageService
 import com.upf464.koonsdiary.domain.usecase.ResultUseCase
 import javax.inject.Inject
 
 internal class AutoSignInUseCase @Inject constructor(
-    val userRepository: UserRepository,
-    val kakaoService: KakaoService
+    private val userRepository: UserRepository,
+    private val kakaoService: KakaoService,
+    private val messageService: MessageService,
+    private val messageRepository: MessageRepository
 ) : ResultUseCase<AutoSignInRequest, EmptyResponse> {
 
     override suspend fun invoke(request: AutoSignInRequest): Result<EmptyResponse> {
@@ -22,6 +26,10 @@ internal class AutoSignInUseCase @Inject constructor(
                 SignInType.KAKAO -> autoSignInWithKakao()
                 null -> Result.failure(SignInError.NoAutoSignIn)
             }
+        }.flatMap {
+            messageService.getToken()
+        }.flatMap { token ->
+            messageRepository.registerFcmToken(token)
         }.map {
             EmptyResponse
         }.onFailure { error ->
--- firebase/src/main/java/com/upf464/koonsdiary/firebase/service/MessageServiceImpl.kt
+++ firebase/src/main/java/com/upf464/koonsdiary/firebase/service/MessageServiceImpl.kt
@@ -1,11 +1,14 @@
 package com.upf464.koonsdiary.firebase.service
 
+import com.google.firebase.messaging.FirebaseMessaging
 import com.upf464.koonsdiary.domain.model.Message
 import com.upf464.koonsdiary.domain.service.MessageService
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.suspendCancellableCoroutine
 import javax.inject.Inject
 import javax.inject.Singleton
+import kotlin.coroutines.resume
 
 @Singleton
 internal class MessageServiceImpl @Inject constructor(
@@ -19,5 +22,17 @@ internal class MessageServiceImpl @Inject constructor(
 
     override fun setMessage(message: Message) {
         messageFlow.tryEmit(message)
+    }
+
+    override suspend fun getToken(): Result<String> = suspendCancellableCoroutine { cancellable ->
+        FirebaseMessaging.getInstance().token.addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                task.result?.let { token ->
+                    cancellable.resume(Result.success(token))
+                }
+            } else {
+                cancellable.resume(Result.failure(Exception("No FCM token")))
+            }
+        }
     }
 }
