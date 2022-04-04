4b3c4591321efca879e0199e6a439a08a6e108a2
Keich.Park <svclaw2000@gmail.com>
2022-04-04 19:57:43 +0900

    feat: KDA-43 PIN 저장 및 조회 repository 추가

---
+++ domain/src/main/java/com/upf464/koonsdiary/domain/error/SecurityError.kt
@@ -0,0 +1,12 @@
+package com.upf464.koonsdiary.domain.error
+
+sealed class SecurityError(
+    message: String? = null,
+    cause: Throwable? = null
+) : Exception(message, cause) {
+
+    data class NoStoredPIN(
+        override val message: String? = null,
+        override val cause: Throwable? = null
+    ) : SecurityError(message, cause)
+}
