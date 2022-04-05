fd6272f31b2c8d851b560b1fc62fa65306b3bea1
JOLLA99 <yoon6148@daum.net>
2022-04-05 17:26:57 +0900

    feat: KDA-58 Reaction Data 레이어에서 의존성 추가

--- data/src/main/java/com/upf464/koonsdiary/data/source/CottonRemoteDataSource.kt
+++ data/src/main/java/com/upf464/koonsdiary/data/source/CottonRemoteDataSource.kt
@@ -2,6 +2,7 @@ package com.upf464.koonsdiary.data.source
 
 import com.upf464.koonsdiary.data.model.QuestionAnswerData
 import com.upf464.koonsdiary.data.model.QuestionData
+import com.upf464.koonsdiary.data.model.ReactionData
 
 interface CottonRemoteDataSource {
 
@@ -10,4 +11,6 @@ interface CottonRemoteDataSource {
     suspend fun addQuestionAnswer(questionAnswer: QuestionAnswerData): Result<Int>
 
     suspend fun fetchRandomAnswer(answerId: Int): Result<QuestionAnswerData>
+
+    suspend fun fetchReaction(): Result<ReactionData>
 }
