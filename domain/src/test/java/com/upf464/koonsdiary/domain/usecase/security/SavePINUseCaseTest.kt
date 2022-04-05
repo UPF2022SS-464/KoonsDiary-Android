7c689b1c1257f28e8ad9f7bd9dfe747dac0b2298
Keich.Park <svclaw2000@gmail.com>
2022-04-05 19:31:31 +0900

    test: KDA-43 PIN 설정 테스트 추가

---
+++ domain/src/test/java/com/upf464/koonsdiary/domain/usecase/security/SavePINUseCaseTest.kt
@@ -0,0 +1,55 @@
+package com.upf464.koonsdiary.domain.usecase.security
+
+import com.upf464.koonsdiary.domain.common.HashGenerator
+import com.upf464.koonsdiary.domain.error.SecurityError
+import com.upf464.koonsdiary.domain.repository.SecurityRepository
+import com.upf464.koonsdiary.domain.request.security.SavePINRequest
+import io.mockk.MockKAnnotations
+import io.mockk.coEvery
+import io.mockk.impl.annotations.MockK
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Test
+
+class SavePINUseCaseTest {
+
+    @MockK private lateinit var securityRepository: SecurityRepository
+    @MockK private lateinit var hashGenerator: HashGenerator
+    private lateinit var useCase: SavePINUseCase
+
+    @Before
+    fun setup() {
+        MockKAnnotations.init(this)
+        useCase = SavePINUseCase(securityRepository, hashGenerator)
+    }
+
+    @Test
+    fun invoke_validPIN_isSuccess(): Unit = runBlocking {
+        coEvery {
+            securityRepository.fetchDisposableSalt()
+        } returns Result.success("salt")
+
+        coEvery {
+            hashGenerator.hashPasswordWithSalt("0000","salt")
+        } returns "pin with salt"
+
+        coEvery {
+            securityRepository.setPIN(any())
+        } returns Result.success(Unit)
+
+        val result = useCase(SavePINRequest("0000"))
+
+        assertTrue(result.isSuccess)
+    }
+
+    @Test
+    fun invoke_invalidPIN_throwInvalidPINError(): Unit = runBlocking {
+
+        val result = useCase(SavePINRequest("000"))
+
+        assertTrue(result.isFailure)
+        assertEquals(SecurityError.InvalidPIN, result.exceptionOrNull())
+    }
+}
