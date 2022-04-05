008e41d60979d0e43e5f50039704d84fb7373435
JOLLA99 <yoon6148@daum.net>
2022-04-05 16:10:12 +0900

    feat: KDA-58 Data 레이어에 레포지토리 및 데이터 소스 추가

--- domain/src/main/java/com/upf464/koonsdiary/domain/repository/CottonRepository.kt
+++ domain/src/main/java/com/upf464/koonsdiary/domain/repository/CottonRepository.kt
@@ -8,4 +8,6 @@ interface CottonRepository {
     suspend fun fetchRandomQuestion(): Result<Question>
 
     suspend fun addQuestionAnswer(questionAnswer: QuestionAnswer): Result<Int>
+
+    suspend fun fetchRandomAnswer(answerId: Int): Result<QuestionAnswer>
 }
