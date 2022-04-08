8be021ff99dab285c8804153c31e302693009bb0
Keich.Park <svclaw2000@gmail.com>
2022-04-08 13:55:28 +0900

    test: KDA-70 프로필 이미지 목록 조회 테스트 추가

---
+++ domain/src/test/java/com/upf464/koonsdiary/domain/usecase/user/FetchUserImageListUseCaseTest.kt
@@ -0,0 +1,35 @@
+package com.upf464.koonsdiary.domain.usecase.user
+
+import com.upf464.koonsdiary.domain.model.User
+import com.upf464.koonsdiary.domain.repository.UserRepository
+import com.upf464.koonsdiary.domain.request.user.FetchUserImageListRequest
+import io.mockk.coEvery
+import io.mockk.impl.annotations.MockK
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Test
+
+class FetchUserImageListUseCaseTest {
+
+    @MockK private lateinit var userRepository: UserRepository
+    private lateinit var useCase: FetchUserImageListUseCase
+
+    @Before
+    fun setup() {
+        useCase = FetchUserImageListUseCase(userRepository)
+    }
+
+    @Test
+    fun invoke_nothing_returnEmptyList(): Unit = runBlocking {
+        coEvery {
+            userRepository.fetchUserImageList()
+        } returns Result.success(emptyList())
+
+        val result = useCase(FetchUserImageListRequest)
+
+        assertTrue(result.isSuccess)
+        assertEquals(emptyList<User.Image>(), result.getOrNull()?.imageList)
+    }
+}
