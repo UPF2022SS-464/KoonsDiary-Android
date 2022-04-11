3e9c83271acfcc72974a39d5927328743bdfc988
Keich.Park <svclaw2000@gmail.com>
2022-04-12 00:38:59 +0900

    feat: KDA-49 이메일 회원가입 샘플 화면 추가

--- presentation/src/main/java/com/upf464/koonsdiary/presentation/MainActivity.kt
+++ presentation/src/main/java/com/upf464/koonsdiary/presentation/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.upf464.koonsdiary.presentation
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.upf464.koonsdiary.presentation.ui.account.EmailSignUpActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -9,5 +11,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        startActivity(Intent(this, EmailSignUpActivity::class.java))
     }
 }
