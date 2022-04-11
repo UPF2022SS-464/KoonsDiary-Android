b1cae15e5a7b70bbd28dc4f602c15cbdcfe11ce8
Keich.Park <svclaw2000@gmail.com>
2022-04-12 00:05:14 +0900

    feat: KDA-49 회원가입 입력란 추가

--- presentation/src/main/java/com/upf464/koonsdiary/presentation/ui/account/EmailSignUpViewModel.kt
+++ presentation/src/main/java/com/upf464/koonsdiary/presentation/ui/account/EmailSignUpViewModel.kt
@@ -8,16 +8,21 @@ import com.upf464.koonsdiary.domain.response.EmptyResponse
 import com.upf464.koonsdiary.domain.usecase.ResultUseCase
 import com.upf464.koonsdiary.presentation.model.account.UserEmailModel
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-@FlowPreview
 internal class EmailSignUpViewModel @Inject constructor(
     private val signUpUseCase: ResultUseCase<SignUpWithUsernameRequest, EmptyResponse>,
     validateUseCase: ResultUseCase<ValidateSignUpRequest, EmptyResponse>
@@ -35,6 +40,27 @@ internal class EmailSignUpViewModel @Inject constructor(
         NICKNAME
     }
 
+    val firstFieldFlow = MutableStateFlow("")
+    @OptIn(ExperimentalCoroutinesApi::class)
+    val firstValidationFlow = pageFlow.flatMapLatest { page ->
+        when (page) {
+            SignUpPage.EMAIL -> userModel.emailValidFlow
+            SignUpPage.USERNAME -> userModel.usernameValidFlow
+            SignUpPage.PASSWORD -> userModel.passwordValidFlow
+            SignUpPage.NICKNAME -> userModel.nicknameValidFlow
+            else -> flow {}
+        }
+    }.stateIn(viewModelScope, SharingStarted.Lazily, UserEmailModel.State.WAITING)
+
+    val secondFieldFlow = MutableStateFlow("")
+    val secondValidationFlow = userModel.passwordConfirmValidFlow
+        .stateIn(viewModelScope, SharingStarted.Lazily, UserEmailModel.State.WAITING)
+
+    private val isNextAvailable
+        get() = firstValidationFlow.value == UserEmailModel.State.SUCCESS &&
+                (pageFlow.value != SignUpPage.PASSWORD ||
+                        secondValidationFlow.value == UserEmailModel.State.SUCCESS)
+
     private val _eventFlow = MutableSharedFlow<SignUpEvent>(extraBufferCapacity = 1)
     val eventFlow: Flow<SignUpEvent> = _eventFlow
 
@@ -45,12 +71,49 @@ internal class EmailSignUpViewModel @Inject constructor(
         object Success : SignUpEvent()
     }
 
+    init {
+        viewModelScope.launch {
+            var job: Job? = null
+            pageFlow.collect { page ->
+                job?.cancel()
+                val targetFlow = when (page) {
+                    SignUpPage.EMAIL -> userModel.emailFlow
+                    SignUpPage.USERNAME -> userModel.usernameFlow
+                    SignUpPage.PASSWORD -> userModel.passwordFlow
+                    SignUpPage.NICKNAME -> userModel.nicknameFlow
+                    SignUpPage.PROFILE -> return@collect
+                }
+                job = connectFieldFlow(targetFlow, firstFieldFlow, this)
+            }
+        }
+
+        connectFieldFlow(userModel.passwordConfirmFlow, secondFieldFlow, viewModelScope)
+    }
+
+    private fun connectFieldFlow(
+        targetFlow: MutableStateFlow<String>,
+        fieldFlow: MutableStateFlow<String>,
+        scope: CoroutineScope
+    ): Job {
+        fieldFlow.value = targetFlow.value
+        return scope.launch {
+            fieldFlow.collect {
+                targetFlow.value = it
+            }
+        }
+    }
+
     fun nextPage() {
+        if (!isNextAvailable) return
+
         val lastPageIdx = SignUpPage.values().size - 1
         val currentPageIdx = _pageFlow.value.ordinal
-        if (currentPageIdx == lastPageIdx) return
 
-        _pageFlow.value = SignUpPage.values()[currentPageIdx + 1]
+        if (currentPageIdx == lastPageIdx) {
+            signUp()
+        } else {
+            _pageFlow.value = SignUpPage.values()[currentPageIdx + 1]
+        }
     }
 
     fun prevPage() {
