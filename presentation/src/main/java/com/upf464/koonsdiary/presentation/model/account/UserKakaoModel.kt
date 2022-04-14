bf48e3409e8f3e643cf90e711d405f7f6df27865
Keich.Park <svclaw2000@gmail.com>
2022-04-14 14:22:00 +0900

    feat: KDA-76 카카오 회원가입 ViewModel 추가

---
+++ presentation/src/main/java/com/upf464/koonsdiary/presentation/model/account/UserKakaoModel.kt
@@ -0,0 +1,51 @@
+package com.upf464.koonsdiary.presentation.model.account
+
+import com.upf464.koonsdiary.domain.error.SignUpError
+import com.upf464.koonsdiary.domain.request.user.ValidateSignUpRequest
+import com.upf464.koonsdiary.domain.response.EmptyResponse
+import com.upf464.koonsdiary.domain.usecase.ResultUseCase
+import com.upf464.koonsdiary.presentation.common.Constants
+import com.upf464.koonsdiary.presentation.mapper.toEmailSignUpState
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.channelFlow
+import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onEach
+
+internal data class UserKakaoModel(
+    val useCase: ResultUseCase<ValidateSignUpRequest, EmptyResponse>,
+    val usernameFlow: MutableStateFlow<String> = MutableStateFlow(""),
+    val imageFlow: MutableStateFlow<UserImageModel?> = MutableStateFlow(null),
+    val nicknameFlow: MutableStateFlow<String> = MutableStateFlow("")
+) {
+
+    val usernameValidFlow: Flow<SignUpState> = waitFirstFlow(usernameFlow) {
+        val error = useCase(ValidateSignUpRequest(ValidateSignUpRequest.Type.USERNAME, it))
+            .exceptionOrNull() ?: return@waitFirstFlow SignUpState.SUCCESS
+        (error as? SignUpError)?.toEmailSignUpState() ?: SignUpState.UNKNOWN
+    }
+
+    @OptIn(FlowPreview::class)
+    private fun waitFirstFlow(source: Flow<String>, mapBlock: suspend (String) -> SignUpState) =
+        channelFlow {
+            source.onEach {
+                send(SignUpState.WAITING)
+            }.debounce(Constants.SIGN_UP_DEBOUNCE_TIME)
+                .map(mapBlock)
+                .collect {
+                    send(it)
+                }
+        }
+
+    val imageValidFlow: Flow<SignUpState> = imageFlow.map { image ->
+        if (image != null) SignUpState.SUCCESS else SignUpState.UNSELECTED_IMAGE
+    }
+
+    val nicknameValidFlow: Flow<SignUpState> = nicknameFlow.map {
+        val error = useCase(ValidateSignUpRequest(ValidateSignUpRequest.Type.NICKNAME, it))
+            .exceptionOrNull() ?: return@map SignUpState.SUCCESS
+        (error as? SignUpError)?.toEmailSignUpState() ?: SignUpState.UNKNOWN
+    }
+}
