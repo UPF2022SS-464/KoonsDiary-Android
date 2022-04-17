cc1e5da3d6caf3dd10ddb3475b0be69f2134a167
Keich.Park <svclaw2000@gmail.com>
2022-04-17 21:56:48 +0900

    feat: KDA-71 로그임 홈 뷰모델 추가

---
+++ presentation/src/main/java/com/upf464/koonsdiary/presentation/ui/account/signin/SignInHomeViewModel.kt
@@ -0,0 +1,60 @@
+package com.upf464.koonsdiary.presentation.ui.account.signin
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.upf464.koonsdiary.domain.error.SignInError
+import com.upf464.koonsdiary.domain.request.user.SignInWithKakaoRequest
+import com.upf464.koonsdiary.domain.response.EmptyResponse
+import com.upf464.koonsdiary.domain.usecase.ResultUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+internal class SignInHomeViewModel @Inject constructor(
+    private val kakaoSignInUseCase: ResultUseCase<SignInWithKakaoRequest, EmptyResponse>
+) : ViewModel() {
+
+    private val _eventFlow = MutableSharedFlow<SignInEvent>(extraBufferCapacity = 1)
+    val eventFlow = _eventFlow.asSharedFlow()
+
+    sealed class SignInEvent {
+
+        object KakaoSignInSuccess : SignInEvent()
+
+        object KakaoSignUp : SignInEvent()
+
+        object EmailSignIn : SignInEvent()
+
+        object EmailSignUp : SignInEvent()
+
+        object UnknownError : SignInEvent()
+    }
+
+    fun signInWithEmail() {
+        setEvent(SignInEvent.EmailSignIn)
+    }
+
+    fun signInWithKakao() {
+        viewModelScope.launch {
+            kakaoSignInUseCase(SignInWithKakaoRequest).onSuccess {
+                setEvent(SignInEvent.KakaoSignInSuccess)
+            }.onFailure { error ->
+                when (error) {
+                    SignInError.NoSuchKakaoUser -> setEvent(SignInEvent.KakaoSignUp)
+                    else -> setEvent(SignInEvent.UnknownError)
+                }
+            }
+        }
+    }
+
+    fun signUpWithEmail() {
+        setEvent(SignInEvent.EmailSignUp)
+    }
+
+    private fun setEvent(event: SignInEvent) {
+        _eventFlow.tryEmit(event)
+    }
+}
