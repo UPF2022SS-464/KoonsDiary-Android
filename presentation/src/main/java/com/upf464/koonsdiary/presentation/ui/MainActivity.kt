3fe37e36af47d0ecfbb9c666cb8702f5a3e24f95
Keich.Park <svclaw2000@gmail.com>
2022-04-19 01:55:36 +0900

    feat: KDA-71 메인 화면 Compose로 변경

--- presentation/src/main/java/com/upf464/koonsdiary/presentation/ui/MainActivity.kt
+++ presentation/src/main/java/com/upf464/koonsdiary/presentation/ui/MainActivity.kt
@@ -2,8 +2,8 @@ package com.upf464.koonsdiary.presentation.ui
 
 import android.content.Intent
 import android.os.Bundle
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import com.upf464.koonsdiary.presentation.R
 import com.upf464.koonsdiary.presentation.ui.account.SignInActivity
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -11,7 +11,7 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {  }
 
         startActivity(Intent(this, SignInActivity::class.java))
     }
