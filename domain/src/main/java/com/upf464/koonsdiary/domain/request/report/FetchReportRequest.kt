30006e537ac22267d1f864502bce8d1d842517e5
JOLLA99 <yoon6148@daum.net>
2022-04-18 15:38:21 +0900

    feat: KDA-44 보고서 조회 UseCase 및 request, response 수정

--- domain/src/main/java/com/upf464/koonsdiary/domain/request/report/FetchReportRequest.kt
+++
@@ -1,9 +0,0 @@
-package com.upf464.koonsdiary.domain.request.report
-
-import com.upf464.koonsdiary.domain.request.Request
-
-data class FetchReportRequest(
-    val period: String,
-    val weekly: String,
-    val date: String
-) : Request
