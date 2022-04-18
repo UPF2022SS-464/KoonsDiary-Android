6a411aa9f914794d2467ec703fe4003b5a871988
Keich.Park <svclaw2000@gmail.com>
2022-04-18 22:04:34 +0900

    feat: KDA-71 Compose 기본 요소 추가

---
+++ presentation/src/main/java/com/upf464/koonsdiary/presentation/ui/theme/Color.kt
@@ -0,0 +1,7 @@
+package com.upf464.koonsdiary.presentation.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val ColorPrimary = Color(0xFF6200EE)
+val DarkGray = Color(0xFF202020)
+val MediumGray = Color(0xFF505050)
