1423a9311baf477dbe1c62bbf379f96b60278ff4
Keich.Park <svclaw2000@gmail.com>
2022-04-18 15:19:28 +0900

    refactor: KDA-71 MainActivity 이동

--- presentation/src/main/java/com/upf464/koonsdiary/presentation/ui/MainActivity.kt
+++ presentation/src/main/java/com/upf464/koonsdiary/presentation/ui/MainActivity.kt
@@ -1,8 +1,9 @@
-package com.upf464.koonsdiary.presentation
+package com.upf464.koonsdiary.presentation.ui
 
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.upf464.koonsdiary.presentation.R
 import com.upf464.koonsdiary.presentation.ui.account.signup.KakaoSignUpActivity
 import dagger.hilt.android.AndroidEntryPoint
 
