0201d28bc430e73c49515fac0894b7e890837b93
JOLLA99 <yoon6148@daum.net>
2022-04-18 15:39:45 +0900

    test: KDA-44 GetReportUseCase 테스트 추가

---
+++ domain/src/test/java/com/upf464/koonsdiary/domain/usecase/report/GetReportUseCaseTest.kt
@@ -0,0 +1,96 @@
+package com.upf464.koonsdiary.domain.usecase.report
+
+import com.upf464.koonsdiary.domain.error.ReportError
+import com.upf464.koonsdiary.domain.model.DateTerm
+import com.upf464.koonsdiary.domain.model.Sentiment
+import com.upf464.koonsdiary.domain.repository.ReportRepository
+import com.upf464.koonsdiary.domain.request.report.GetReportRequest
+import io.mockk.MockKAnnotations
+import io.mockk.coEvery
+import io.mockk.impl.annotations.MockK
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Test
+import java.time.LocalDate
+
+class GetReportUseCaseTest {
+
+    @MockK
+    private lateinit var reportRepository: ReportRepository
+    private lateinit var useCase: GetReportUseCase
+
+    @Before
+    fun setup() {
+        MockKAnnotations.init(this)
+        useCase = GetReportUseCase(
+            reportRepository = reportRepository
+        )
+        coEvery {
+            reportRepository.fetchAllSentiment()
+        } returns Result.success(
+            mapOf(
+                LocalDate.of(2022, 4, 4) to Sentiment.NORMAL,
+                LocalDate.of(2022, 4, 5) to Sentiment.NORMAL,
+                LocalDate.of(2022, 4, 6) to Sentiment.NORMAL,
+                LocalDate.of(2022, 4, 7) to Sentiment.NORMAL,
+                LocalDate.of(2022, 4, 11) to Sentiment.NORMAL,
+                LocalDate.of(2022, 4, 18) to Sentiment.NORMAL,
+                LocalDate.of(2022, 5, 11) to Sentiment.NORMAL,
+                LocalDate.of(2022, 5, 18) to Sentiment.NORMAL,
+                LocalDate.of(2022, 5, 25) to Sentiment.NORMAL,
+            )
+        )
+
+    }
+
+    @Test
+    fun invoke_dayReport_isSuccess(): Unit = runBlocking {
+
+        coEvery {
+            reportRepository.fetchKoonsMention(sentiment = Sentiment.NORMAL)
+        } returns Result.success("")
+
+        val result = useCase(GetReportRequest(DateTerm.DAY_7, LocalDate.of(2022, 4, 11)))
+
+        assertTrue(result.isSuccess)
+    }
+
+    @Test
+    fun invoke_weekReport_isSuccess(): Unit = runBlocking {
+
+        coEvery {
+            reportRepository.fetchKoonsMention(sentiment = Sentiment.NORMAL)
+        } returns Result.success("")
+
+        val result = useCase(GetReportRequest(DateTerm.WEEK_14, LocalDate.of(2022, 4, 11)))
+
+        assertTrue(result.isSuccess)
+    }
+
+    @Test
+    fun invoke_monthReport_isSuccess(): Unit = runBlocking {
+
+        coEvery {
+            reportRepository.fetchKoonsMention(sentiment = Sentiment.NORMAL)
+        } returns Result.success("")
+
+        val result = useCase(GetReportRequest(DateTerm.MONTH_28, LocalDate.of(2022, 4, 11)))
+
+        assertTrue(result.isSuccess)
+    }
+
+    @Test
+    fun invoke_emptySentiment_throwsNoSentimentError(): Unit = runBlocking {
+
+        coEvery {
+            reportRepository.fetchKoonsMention(any())
+        } returns Result.failure(ReportError.NoSentiment)
+
+        val result = useCase(GetReportRequest(DateTerm.MONTH_28, LocalDate.of(2022, 4, 11)))
+
+        assertTrue(result.isFailure)
+        assertEquals(ReportError.NoSentiment, result.exceptionOrNull())
+    }
+}
