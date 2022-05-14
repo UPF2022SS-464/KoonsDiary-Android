00d0c0ba69f32bcf7b20efb5c719356d56e3b02c
Keich.Park <svclaw2000@gmail.com>
2022-05-14 21:35:10 +0900

    feat: KDA-53 일기 상세 화면과 연결

--- presentation/src/main/java/com/upf464/koonsdiary/presentation/ui/main/diary/detail/DiaryDetailEvent.kt
+++ presentation/src/main/java/com/upf464/koonsdiary/presentation/ui/main/diary/detail/DiaryDetailEvent.kt
@@ -1,10 +1,5 @@
 package com.upf464.koonsdiary.presentation.ui.main.diary.detail
 
 sealed class DiaryDetailEvent {
-
-    object Deleted : DiaryDetailEvent()
-
-    object DeletionError : DiaryDetailEvent()
-
     object Edit : DiaryDetailEvent()
 }
