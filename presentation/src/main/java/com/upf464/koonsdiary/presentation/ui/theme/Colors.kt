5d3b78eb8228360f32aa876723955a1a2400f2cf
Keich.Park <svclaw2000@gmail.com>
2022-05-15 10:52:45 +0900

    feat: KDA-79 테마값 추가

--- presentation/src/main/java/com/upf464/koonsdiary/presentation/ui/theme/Colors.kt
+++ presentation/src/main/java/com/upf464/koonsdiary/presentation/ui/theme/Colors.kt
@@ -11,6 +11,21 @@ val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
 val Black100 = Color(0xFF15141F)
 
+object KoonsColor {
+    val Green = Color(0xFFB5D3B6)
+    val Red = Color(0xFFEB7487)
+
+    val Black100 = Color(0xFF15141F)
+    val Black60 = Color(0xFFA2A0A8)
+    val Black40 = Color(0xFFCCCACF)
+    val Black20 = Color(0xFFDCDBE0)
+    val Black10 = Color(0xFFE8E8E8)
+    val Black5 = Color(0xFFF9F9FA)
+
+    val KakaoBackground = Color(0xFFF7E600)
+    val KakaoContent = Color(0xFF3A1D1D)
+}
+
 @Composable
 fun transparentTextColors() = TextFieldDefaults.textFieldColors(
     backgroundColor = Color.Transparent,
