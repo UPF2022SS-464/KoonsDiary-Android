ee43fd648a834b7fdd6d155241c181937a25d9d6
Keich.Park <svclaw2000@gmail.com>
2022-05-25 22:36:59 +0900

    feat: KDA-51 공유그룹 그리드 추가

--- presentation/src/main/java/com/upf464/koonsdiary/presentation/ui/main/share/group_list/ShareGroupListScreen.kt
+++ presentation/src/main/java/com/upf464/koonsdiary/presentation/ui/main/share/group_list/ShareGroupListScreen.kt
@@ -1,5 +1,7 @@
 package com.upf464.koonsdiary.presentation.ui.main.share.group_list
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -7,6 +9,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
@@ -15,7 +19,9 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.GridCells
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.LazyVerticalGrid
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -33,6 +39,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import coil.compose.AsyncImage
@@ -40,7 +47,6 @@ import com.google.accompanist.pager.ExperimentalPagerApi
 import com.google.accompanist.pager.HorizontalPager
 import com.google.accompanist.pager.rememberPagerState
 import com.upf464.koonsdiary.domain.model.ShareGroup
-import com.upf464.koonsdiary.domain.model.User
 import com.upf464.koonsdiary.presentation.R
 import com.upf464.koonsdiary.presentation.ui.theme.KoonsColor
 import com.upf464.koonsdiary.presentation.ui.theme.KoonsTypography
@@ -50,57 +56,7 @@ internal fun ShareGroupListScreen(
     viewModel: ShareGroupListViewModel = hiltViewModel()
 ) {
     ShareGroupListScreen(
-//        groupListState = viewModel.groupListStateFlow.collectAsState().value,
-        groupListState = ShareGroupListState.Success(
-            listOf(
-                ShareGroup(
-                    name = "테스트 그룹1",
-                    imagePath = "https://i.pinimg.com/originals/3f/ba/d9/3fbad97c5829c3df9d857dae7857c7ce.jpg",
-                    userList = listOf(
-                        User(
-                            username = "username1",
-                            nickname = "닉네임1",
-                            image = User.Image(path = "https://i.pinimg.com/originals/3f/ba/d9/3fbad97c5829c3df9d857dae7857c7ce.jpg")
-                        ),
-                        User(
-                            username = "username2",
-                            nickname = "닉네임2",
-                            image = User.Image(path = "https://i.pinimg.com/originals/3f/ba/d9/3fbad97c5829c3df9d857dae7857c7ce.jpg")
-                        ),
-                        User(
-                            username = "username3",
-                            nickname = "닉네임3",
-                            image = User.Image(path = "https://i.pinimg.com/originals/3f/ba/d9/3fbad97c5829c3df9d857dae7857c7ce.jpg")
-                        )
-                    )
-                ),
-                ShareGroup(
-                    name = "테스트 그룹2",
-                    imagePath = "https://i.pinimg.com/originals/3f/ba/d9/3fbad97c5829c3df9d857dae7857c7ce.jpg",
-                    userList = listOf(
-                        User(
-                            username = "username4",
-                            nickname = "닉네임4",
-                            image = User.Image(path = "https://i.pinimg.com/originals/3f/ba/d9/3fbad97c5829c3df9d857dae7857c7ce.jpg")
-                        ),
-                        User(
-                            username = "username5",
-                            nickname = "닉네임5",
-                            image = User.Image(path = "https://i.pinimg.com/originals/3f/ba/d9/3fbad97c5829c3df9d857dae7857c7ce.jpg")
-                        ),
-                        User(
-                            username = "username6",
-                            nickname = "닉네임6",
-                            image = User.Image(path = "https://i.pinimg.com/originals/3f/ba/d9/3fbad97c5829c3df9d857dae7857c7ce.jpg")
-                        )
-                    )
-                ),
-                ShareGroup(
-                    name = "테스트 그룹3",
-                    imagePath = "https://i.pinimg.com/originals/3f/ba/d9/3fbad97c5829c3df9d857dae7857c7ce.jpg"
-                ),
-            )
-        ),
+        groupListState = viewModel.groupListStateFlow.collectAsState().value,
         viewType = viewModel.viewTypeFlow.collectAsState().value,
         onChangeViewType = { viewModel.changeViewType(it) }
     )
@@ -231,86 +187,19 @@ private fun ColumnScope.ShareGroupPager(
         state = pagerState
     ) { index ->
         if (index == 0) {
-            Card(
-                backgroundColor = KoonsColor.Black5,
-                shape = RoundedCornerShape(16.dp),
-                modifier = Modifier
-                    .padding(horizontal = 48.dp)
-                    .fillMaxWidth()
-            ) {
-                Box(modifier = Modifier.height(IntrinsicSize.Min)) {
-                    Column(
-                        modifier = Modifier.padding(vertical = 48.dp),
-                        horizontalAlignment = Alignment.CenterHorizontally
-                    ) {
-                        Box(
-                            modifier = Modifier
-                                .padding(horizontal = 32.dp)
-                                .fillMaxWidth()
-                                .aspectRatio(1f)
-                                .clip(RoundedCornerShape(12.dp))
-                                .background(KoonsColor.Black40)
-                        )
-                        Text(
-                            text = "새 공유 일기장 만들기",
-                            style = KoonsTypography.H7,
-                            color = KoonsColor.Black100,
-                            modifier = Modifier.padding(top = 16.dp)
-                        )
-                    }
-
-                    Box(
-                        modifier = Modifier
-                            .fillMaxHeight()
-                            .align(Alignment.CenterEnd)
-                            .padding(end = 48.dp)
-                            .width(12.dp)
-                            .background(KoonsColor.Green)
-                    )
-                }
-            }
+            ShareGroupItem(
+                viewType = ShareGroupListViewType.PAGER,
+                model = null,
+                paddingHorizontal = 32.dp,
+                paddingVertical = 48.dp
+            )
         } else {
-            val model = groupList[index - 1]
-            Card(
-                backgroundColor = KoonsColor.Black5,
-                shape = RoundedCornerShape(16.dp),
-                modifier = Modifier
-                    .padding(horizontal = 48.dp)
-                    .fillMaxWidth()
-            ) {
-                Box(modifier = Modifier.height(IntrinsicSize.Min)) {
-                    Column(
-                        modifier = Modifier.padding(vertical = 48.dp),
-                        horizontalAlignment = Alignment.CenterHorizontally
-                    ) {
-                        AsyncImage(
-                            model = model.imagePath,
-                            contentDescription = null,
-                            contentScale = ContentScale.Crop,
-                            modifier = Modifier
-                                .padding(horizontal = 32.dp)
-                                .fillMaxWidth()
-                                .aspectRatio(1f)
-                                .clip(RoundedCornerShape(12.dp))
-                        )
-                        Text(
-                            text = model.name,
-                            style = KoonsTypography.H7,
-                            color = KoonsColor.Black100,
-                            modifier = Modifier.padding(top = 16.dp)
-                        )
-                    }
-
-                    Box(
-                        modifier = Modifier
-                            .fillMaxHeight()
-                            .align(Alignment.CenterEnd)
-                            .padding(end = 48.dp)
-                            .width(12.dp)
-                            .background(KoonsColor.Green)
-                    )
-                }
-            }
+            ShareGroupItem(
+                viewType = ShareGroupListViewType.PAGER,
+                model = groupList[index - 1],
+                paddingHorizontal = 32.dp,
+                paddingVertical = 48.dp
+            )
         }
     }
 
@@ -318,8 +207,97 @@ private fun ColumnScope.ShareGroupPager(
 }
 
 @Composable
+@OptIn(ExperimentalFoundationApi::class)
 private fun ShareGroupGrid(
     groupList: List<ShareGroup>
 ) {
+    LazyVerticalGrid(
+        cells = GridCells.Fixed(2),
+        contentPadding = PaddingValues(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+        horizontalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        item {
+            ShareGroupItem(
+                viewType = ShareGroupListViewType.GRID,
+                model = null,
+                paddingHorizontal = 24.dp,
+                paddingVertical = 32.dp
+            )
+        }
 
+        items(groupList) { model ->
+            ShareGroupItem(
+                viewType = ShareGroupListViewType.GRID,
+                model = model,
+                paddingHorizontal = 24.dp,
+                paddingVertical = 32.dp
+            )
+        }
+    }
+}
+
+@Composable
+private fun ShareGroupItem(
+    viewType: ShareGroupListViewType,
+    model: ShareGroup?,
+    paddingHorizontal: Dp,
+    paddingVertical: Dp
+) {
+    Card(
+        backgroundColor = KoonsColor.Black5,
+        shape = RoundedCornerShape(16.dp),
+        border = BorderStroke(1.dp, KoonsColor.Black100),
+        modifier = when (viewType) {
+            ShareGroupListViewType.PAGER -> Modifier.padding(horizontal = 48.dp)
+            ShareGroupListViewType.GRID -> Modifier
+        }.fillMaxWidth()
+    ) {
+        Box(modifier = Modifier.height(IntrinsicSize.Min)) {
+            Column(
+                modifier = Modifier.padding(vertical = paddingVertical),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                if (model == null) {
+                    Box(
+                        modifier = Modifier
+                            .padding(horizontal = paddingHorizontal)
+                            .fillMaxWidth()
+                            .aspectRatio(1f)
+                            .clip(RoundedCornerShape(12.dp))
+                            .background(KoonsColor.Black40)
+                    )
+                } else {
+                    AsyncImage(
+                        model = model.imagePath,
+                        contentDescription = null,
+                        contentScale = ContentScale.Crop,
+                        modifier = Modifier
+                            .padding(horizontal = paddingHorizontal)
+                            .fillMaxWidth()
+                            .aspectRatio(1f)
+                            .clip(RoundedCornerShape(12.dp))
+                    )
+                }
+
+                Text(
+                    text = model?.name ?: "새 공유 일기장 만들기",
+                    style = KoonsTypography.H7,
+                    color = KoonsColor.Black100,
+                    modifier = Modifier.padding(top = 16.dp)
+                )
+            }
+
+            Row(modifier = Modifier.fillMaxWidth()) {
+                Spacer(modifier = Modifier.weight(6f))
+                Box(
+                    modifier = Modifier
+                        .fillMaxHeight()
+                        .width(12.dp)
+                        .background(KoonsColor.Green)
+                )
+                Spacer(modifier = Modifier.weight(1f))
+            }
+        }
+    }
 }
