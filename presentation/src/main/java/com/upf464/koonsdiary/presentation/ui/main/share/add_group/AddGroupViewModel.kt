e6ef5d41e8ee390b08cc4e2747ea87e891f0f66c
Keich.Park <svclaw2000@gmail.com>
2022-05-25 23:16:11 +0900

    feat: KDA-51 공유그룹 추가 액티비티 추가

---
+++ presentation/src/main/java/com/upf464/koonsdiary/presentation/ui/main/share/add_group/AddGroupViewModel.kt
@@ -0,0 +1,15 @@
+package com.upf464.koonsdiary.presentation.ui.main.share.add_group
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+internal class AddGroupViewModel @Inject constructor(
+
+) : ViewModel() {
+
+    fun save() {
+
+    }
+}
