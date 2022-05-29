589047f9a8fa0247b428598fe45990d34c926202
Keich.Park <svclaw2000@gmail.com>
2022-05-29 20:57:01 +0900

    feat: KDA-51 공유그룹 추가 초대 부분

--- presentation/src/main/java/com/upf464/koonsdiary/presentation/ui/main/share/add_group/AddGroupScreen.kt
+++ presentation/src/main/java/com/upf464/koonsdiary/presentation/ui/main/share/add_group/AddGroupScreen.kt
@@ -3,9 +3,11 @@ package com.upf464.koonsdiary.presentation.ui.main.share.add_group
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
@@ -14,12 +16,18 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Card
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Divider
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.Text
@@ -31,10 +39,14 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import coil.compose.AsyncImage
+import com.upf464.koonsdiary.domain.model.User
+import com.upf464.koonsdiary.presentation.R
 import com.upf464.koonsdiary.presentation.ui.theme.KoonsColor
 import com.upf464.koonsdiary.presentation.ui.theme.KoonsTypography
 
@@ -53,7 +65,14 @@ private fun AddGroupScreen(
     imagePath: String? = null,
     groupName: String = "",
     onGroupNameChange: (String) -> Unit = { },
-    onImageClicked: () -> Unit = { }
+    onImageClicked: () -> Unit = { },
+    inviteUserList: List<User> = listOf(),
+    onInviteDeleteClicked: (Int) -> Unit = { },
+    keyword: String = "",
+    onKeywordChanged: (String) -> Unit = { },
+    searchResult: List<User> = listOf(),
+    onInviteClicked: (User) -> Unit = { },
+    isWaitingResult: Boolean = false
 ) {
     Column(modifier = Modifier.fillMaxSize()) {
         AddGroupAppBar(onSave = onSave)
@@ -68,6 +87,19 @@ private fun AddGroupScreen(
                 groupName = groupName,
                 onGroupNameChange = onGroupNameChange,
                 onImageClicked = onImageClicked
+            )
+
+            InviteUserRow(
+                inviteUserList = inviteUserList,
+                onDeleteClicked = onInviteDeleteClicked
+            )
+
+            InviteUserSearch(
+                keyword = keyword,
+                onKeywordChanged = onKeywordChanged,
+                searchResult = searchResult,
+                onInviteClicked = onInviteClicked,
+                isWaitingResult = isWaitingResult
             )
         }
     }
@@ -172,4 +204,141 @@ private fun GroupInformation(
             }
         }
     }
+}
+
+@Composable
+private fun InviteUserRow(
+    inviteUserList: List<User>,
+    onDeleteClicked: (Int) -> Unit
+) {
+    LazyRow(
+        contentPadding = PaddingValues(horizontal = 24.dp),
+        horizontalArrangement = Arrangement.spacedBy(16.dp),
+        modifier = Modifier
+            .padding(top = 48.dp)
+            .fillMaxWidth()
+    ) {
+        items(inviteUserList.size) { index ->
+            val item = inviteUserList[index]
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Box {
+                    AsyncImage(
+                        model = item.image.path,
+                        contentDescription = null,
+                        contentScale = ContentScale.Crop,
+                        modifier = Modifier
+                            .size(64.dp)
+                            .clip(CircleShape)
+                    )
+
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_delete),
+                        contentDescription = null,
+                        tint = KoonsColor.Red,
+                        modifier = Modifier
+                            .clickable { onDeleteClicked(index) }
+                            .align(Alignment.TopEnd)
+                    )
+                }
+                Text(
+                    text = item.nickname,
+                    style = KoonsTypography.BodyMoreSmall,
+                    color = KoonsColor.Black100,
+                    modifier = Modifier.padding(top = 4.dp)
+                )
+            }
+        }
+    }
+}
+
+@Composable
+@OptIn(ExperimentalMaterialApi::class)
+private fun InviteUserSearch(
+    keyword: String,
+    onKeywordChanged: (String) -> Unit,
+    searchResult: List<User>,
+    onInviteClicked: (User) -> Unit,
+    isWaitingResult: Boolean
+) {
+    Text(
+        text = "초대할 친구",
+        style = KoonsTypography.H8,
+        color = KoonsColor.Black100,
+        modifier = Modifier.padding(horizontal = 32.dp)
+    )
+
+    BasicTextField(
+        value = keyword,
+        onValueChange = onKeywordChanged,
+        textStyle = KoonsTypography.BodyMedium.copy(color = KoonsColor.Black100),
+        singleLine = true,
+        decorationBox = { innerTextField ->
+            Column {
+                Row(
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    innerTextField()
+                    if (keyword.isNotEmpty()) {
+                        Text(
+                            text = "초대할 아이디를 입력해주세요.",
+                            style = KoonsTypography.BodyMedium,
+                            color = KoonsColor.Black40
+                        )
+                    }
+                    if (isWaitingResult) {
+                        CircularProgressIndicator(
+                            modifier = Modifier.size(
+                                with(LocalDensity.current) {
+                                    KoonsTypography.BodyMedium.fontSize.toDp()
+                                }
+                            ),
+                            strokeWidth = 2.dp
+                        )
+                    }
+                }
+                Divider(color = KoonsColor.Black100, modifier = Modifier.padding(top = 4.dp))
+            }
+        },
+        modifier = Modifier.padding(start = 32.dp, end = 32.dp, top = 12.dp)
+    )
+
+    searchResult.forEach { user ->
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 32.dp, end = 32.dp, top = 12.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            AsyncImage(
+                model = user.image.path,
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .size(24.dp)
+                    .clip(CircleShape)
+            )
+            Text(
+                text = user.username,
+                color = KoonsColor.Black100,
+                style = KoonsTypography.BodyMedium,
+                modifier = Modifier.padding(start = 4.dp)
+            )
+            Spacer(modifier = Modifier.weight(1f))
+            Text(
+                text = "초대하기",
+                style = KoonsTypography.BodyMedium,
+                color = KoonsColor.Black100,
+                modifier = Modifier
+                    .clip(RoundedCornerShape(4.dp))
+                    .background(KoonsColor.Black40)
+                    .clickable { onInviteClicked(user) }
+                    .padding(vertical = 2.dp, horizontal = 16.dp)
+            )
+        }
+    }
+
+    Spacer(modifier = Modifier.height(32.dp))
 }
