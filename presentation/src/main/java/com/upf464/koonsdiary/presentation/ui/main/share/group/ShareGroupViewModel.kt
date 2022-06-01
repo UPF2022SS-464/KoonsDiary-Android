980862504c591b892fe85ff188c9b1499e6c95d2
Keich.Park <svclaw2000@gmail.com>
2022-06-01 18:30:02 +0900

    feat: KDA-52 공유일기 그룹에 일기 목록 추가

--- presentation/src/main/java/com/upf464/koonsdiary/presentation/ui/main/share/group/ShareGroupViewModel.kt
+++ presentation/src/main/java/com/upf464/koonsdiary/presentation/ui/main/share/group/ShareGroupViewModel.kt
@@ -1,12 +1,13 @@
 package com.upf464.koonsdiary.presentation.ui.main.share.group
 
 import androidx.lifecycle.ViewModel
+import com.upf464.koonsdiary.presentation.common.DateTimeUtil
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class ShareGroupViewModel @Inject constructor(
-
+    val dateTimeUtil: DateTimeUtil
 ) : ViewModel() {
 
 
