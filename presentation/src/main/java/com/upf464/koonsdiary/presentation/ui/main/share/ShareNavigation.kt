1d7832fc40a7a51af0a64e25e7e3155835af8530
Keich.Park <svclaw2000@gmail.com>
2022-06-05 11:03:25 +0900

    refactor: KDA-52 Route 인터페이스 추가

--- presentation/src/main/java/com/upf464/koonsdiary/presentation/ui/main/share/ShareNavigation.kt
+++ presentation/src/main/java/com/upf464/koonsdiary/presentation/ui/main/share/ShareNavigation.kt
@@ -1,6 +1,8 @@
 package com.upf464.koonsdiary.presentation.ui.main.share
 
-enum class ShareNavigation(val route: String) {
+import com.upf464.koonsdiary.presentation.model.NavigationRoute
+
+enum class ShareNavigation(override val route: String): NavigationRoute {
     GROUP_LIST("groupList"),
     GROUP_DETAIL("groupDetail"),
     ADD_GROUP("addGroup")
