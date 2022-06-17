47c28a2e2bda90389f09487dc4d6e314597f34b0
Keich.Park <svclaw2000@gmail.com>
2022-06-17 22:16:32 +0900

    feat: KDA-87 색상값 정리

--- presentation/src/main/java/com/upf464/koonsdiary/presentation/ui/theme/Colors.kt
+++ presentation/src/main/java/com/upf464/koonsdiary/presentation/ui/theme/Colors.kt
@@ -9,7 +9,6 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
-val Black100 = Color(0xFF15141F)
 
 object KoonsColor {
     val Green = Color(0xFFB5D3B6)
