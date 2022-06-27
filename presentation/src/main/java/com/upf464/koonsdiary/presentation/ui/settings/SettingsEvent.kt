e76b3b942a4c01058fa01a4f465c6d14969e9d96
Keich.Park <svclaw2000@gmail.com>
2022-06-27 22:40:22 +0900

    feat: KDA-56 생체 인식 설정 기능 추가

--- presentation/src/main/java/com/upf464/koonsdiary/presentation/ui/settings/SettingsEvent.kt
+++ presentation/src/main/java/com/upf464/koonsdiary/presentation/ui/settings/SettingsEvent.kt
@@ -3,4 +3,9 @@ package com.upf464.koonsdiary.presentation.ui.settings
 sealed interface SettingsEvent {
 
     object Finish : SettingsEvent
+    object ShowBiometricFailedError : SettingsEvent
+    object ShowBiometricUnavailableError : SettingsEvent
+    object ShowLockBiometricError : SettingsEvent
+    object ShowNoBiometricError : SettingsEvent
+    object ShowBiometricTimeoutError : SettingsEvent
 }
