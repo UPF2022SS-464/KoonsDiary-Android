898dba56fa0b408eb58209ca3d7560c6bc99b60f
Keich.Park <svclaw2000@gmail.com>
2022-07-03 15:40:49 +0900

    build: KDA-95 data 모듈 kts로 변경

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -1,4 +1,13 @@
 object Dependencies {
+    const val COMMON = "com.upf464.koonsdiary:common:${Version.COMMON}"
+    const val DOMAIN = "com.upf464.koonsdiary:domain:${Version.DOMAIN}"
+    const val DATA = "com.upf464.koonsdiary:data:${Version.DATA}"
+    const val LOCAL = "com.upf464.koonsdiary:local:${Version.LOCAL}"
+    const val REMOTE = "com.upf464.koonsdiary:remote:${Version.REMOTE}"
+    const val PRESENTATION = "com.upf464.koonsdiary:presentation:${Version.PRESENTATION}"
+    const val KAKAO = "com.upf464.koonsdiary:kakao:${Version.KAKAO}"
+    const val FIREBASE = "com.upf464.koonsdiary:firebase:${Version.FIREBASE}"
+
     const val JGIT = "org.eclipse.jgit:org.eclipse.jgit:${Version.JGIT}"
     const val HILT_CORE = "com.google.dagger:hilt-core:${Version.HILT}"
     const val HILT_KAPT = "com.google.dagger:hilt-android-compiler:${Version.HILT}"
@@ -7,6 +16,15 @@ object Dependencies {
     const val COROUTINES_CORE = "org.jetbrains.kotlinx:kotlinx-coroutines-core:${Version.COROUTINES}"
 
     private object Version {
+        const val COMMON = "0.1.0"
+        const val DOMAIN = "0.1.0"
+        const val DATA = "0.1.0"
+        const val LOCAL = "0.1.0"
+        const val REMOTE = "0.1.0"
+        const val PRESENTATION = "0.1.0"
+        const val KAKAO = "0.1.0"
+        const val FIREBASE = "0.1.0"
+
         const val JGIT = "6.2.0.202206071550-r"
         const val HILT = "2.41"
         const val JUNIT = "4.13.2"
