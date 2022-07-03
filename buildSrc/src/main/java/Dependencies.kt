062027a39b8607c7da5f52262c3e8e00592d9820
Keich.Park <svclaw2000@gmail.com>
2022-07-03 15:10:54 +0900

    build: KDA-95 common 모듈 kts로 변경

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -1,7 +1,16 @@
 object Dependencies {
     const val JGIT = "org.eclipse.jgit:org.eclipse.jgit:${Version.JGIT}"
+    const val HILT_CORE = "com.google.dagger:hilt-core:${Version.HILT}"
+    const val HILT_KAPT = "com.google.dagger:hilt-android-compiler:${Version.HILT}"
+    const val JUNIT = "junit:junit:${Version.JUNIT}"
+    const val MOCKK = "io.mockk:mockk:${Version.MOCKK}"
+    const val COROUTINES_CORE = "org.jetbrains.kotlinx:kotlinx-coroutines-core:${Version.COROUTINES}"
 
     private object Version {
         const val JGIT = "6.2.0.202206071550-r"
+        const val HILT = "2.41"
+        const val JUNIT = "4.13.2"
+        const val MOCKK = "1.12.3"
+        const val COROUTINES = "1.6.0"
     }
 }
---
+++ common/build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    id("java-library")
+    id("org.jetbrains.kotlin.jvm")
+    id("kotlin-kapt")
+}
+
+version = "0.1.0"
+apply(from = "../kotlin_publish.gradle")
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+dependencies {
+
+    implementation(Dependencies.HILT_CORE)
+    kapt(Dependencies.HILT_KAPT)
+
+    testImplementation(Dependencies.JUNIT)
+    testImplementation(Dependencies.MOCKK)
+
+    implementation(Dependencies.COROUTINES_CORE)
+}
