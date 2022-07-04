74fb99394af876d64049e5179552882e3dbfa3d7
Keich.Park <svclaw2000@gmail.com>
2022-07-04 23:45:38 +0900

    build: KDA-95 settings.gradle kts로 변경

---
+++ settings.gradle.kts
@@ -0,0 +1,43 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+}
+
+val githubProperties = java.util.Properties().apply {
+    load(java.io.FileInputStream(File("local.properties")))
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven {
+            url = uri("https://devrepo.kakao.com/nexus/content/groups/public/")
+        }
+        maven {
+            name = "GithubPackages"
+            url = uri("https://maven.pkg.github.com/UPF2022SS-464/KoonsDiary-Android")
+            credentials {
+                username = githubProperties["github_username"]?.toString() ?: System.getenv("github_username").toString()
+                password = githubProperties["github_access_token"]?.toString() ?: System.getenv("github_access_token").toString()
+            }
+        }
+    }
+}
+
+rootProject.name = "Koon's Diary"
+include(
+    ":app",
+    ":domain",
+    ":data",
+    ":local",
+    ":remote",
+    ":presentation",
+    ":kakao",
+    ":firebase",
+    ":common"
+)
